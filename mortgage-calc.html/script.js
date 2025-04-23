56290b14bd29d5c9f923924032235678105ad54b
Lucius Jackson <Jackson.l@stu.janesville.k12.wi.us>
2025-04-23 14:43:48 -0500

    4/23/25

--- mortgage-calc.html/script.js
+++ mortgage-calc.html/script.js
@@ -1,1 +1,28 @@
- 
+ function reset() { document.getElementById('output').innerHTML = ""; }
+
+function calculate() {
+    let P = Number(document.getElementById('princ').value);
+    let R = Number(document.getElementById('inter').value);
+    let N = 12;
+    let T = Number(document.getElementById('num').value);
+    let M = Number(document.getElementById('month').value);
+
+// Variable //
+    
+    let NT = N * T
+
+// Part 1 //
+
+    let step1 = R / N 
+    let step2 = step1 + 1
+    let step3 = step2 * NT
+    let step4 = step3 * P
+
+// Part 2 //
+
+    let step5 = 
+
+
+
+// Final Product //
+}
